b8e0daaa96c5f8874a5ea81bcf5c4f9b3ae2e520
Maestro-nk <bovito2005@gmail.com>
2025-11-19 14:44:39 +0200

    Add files via upload

---
+++ LR3OOP2/LR3OOP2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Diagnostics.Tracing;
+using System.Text.Json;
+
+#pragma warning disable SYSLIB0011
+
+namespace LR3OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Vector> vectorsToSave = new List<Vector>{
+             new Vector("Red", 45, 10),
+             new Vector("Blue", 90, 5),
+             new Vector("Green", 0, 15),
+             new Vector("Black", 180, 8),
+             new Vector("Yellow", 270, 12)
+             };
+
+         BinaryFormatter formatter = new BinaryFormatter();
+
+         using (FileStream fs = new FileStream("Vector.bin", FileMode.Create))
+         {
+            formatter.Serialize(fs, vectorsToSave);
+         }
+            Console.WriteLine("Saved to Vector.bin");
+
+            List<Vector> loadedVectors = null;
+            BinaryFormatter loadFormatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("Vector.bin", FileMode.Open))
+            {
+                object loadedData = loadFormatter.Deserialize(fs);
+                loadedVectors = loadedData as List<Vector>;
+            }
+            if (loadedVectors != null)
+            {
+                Console.WriteLine("\nDownloaded from Vector.bin:");
+                foreach (var vector in loadedVectors)
+                {
+                    vector.DisplayInfo();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nDownload error");
+            }
+
+            XmlSerializer xmlFormatter = new XmlSerializer(typeof(List<Vector>));
+
+            using (FileStream fs = new FileStream("Vectors.xml", FileMode.Create))
+            {
+                xmlFormatter.Serialize(fs, vectorsToSave);
+            }
+            Console.WriteLine("\nSaved to Vectors.xml");
+
+            List<Vector> loadedXmlVectors = null;
+
+            XmlSerializer xmlLoader = new XmlSerializer(typeof(List<Vector>));
+
+            using (FileStream fs = new FileStream("Vectors.xml", FileMode.Open))
+            {
+                loadedXmlVectors = (List<Vector>)xmlLoader.Deserialize(fs);
+            }
+
+            if (loadedXmlVectors != null)
+            {
+                Console.WriteLine("\nLoaded from Vectors.xml:");
+                foreach (var vector in loadedXmlVectors)
+                {
+                    vector.DisplayInfo(); 
+                    Console.WriteLine("---");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nCan`t load from XML.");
+            }
+            Console.WriteLine("\nStarting JSON serialization...");
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            using (FileStream fs = new FileStream("vectors.json", FileMode.Create))
+            {
+                JsonSerializer.Serialize(fs, vectorsToSave, jsonOptions);
+            }
+
+            Console.WriteLine("Objects saved to vectors.json");
+
+            List<Vector> loadedJsonVectors = null;
+
+            using (FileStream fs = new FileStream("vectors.json", FileMode.Open))
+            {
+                loadedJsonVectors = JsonSerializer.Deserialize<List<Vector>>(fs);
+            }
+
+            if (loadedJsonVectors != null)
+            {
+                Console.WriteLine("\nSuccessfully loaded from vectors.json:");
+                foreach (var vector in loadedJsonVectors)
+                {
+                    vector.DisplayInfo();
+                    Console.WriteLine("---");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nFailed to load data from JSON.");
+            }
+        }
+    }
+}   
---
+++ LR3OOP2/LR3OOP2/Vector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR3OOP2
+{
+    [Serializable] 
+    public class Vector
+    {
+        public string LineColor { get; set; }
+        public double Angle { get; set; }
+        private double _Length { get; set; }
+        public double Length
+        {
+            get { return _Length; }
+            set
+            {
+                if (value < 0)
+                {
+                    _Length = 0;
+                    Console.WriteLine("Length can`t be < 0");
+                }
+                else { _Length = value; }
+            }
+        }
+        public Vector(string color, double angle, double length)
+        {
+            LineColor = color;
+            Angle = angle;
+            Length = length;
+        }
+
+        public Vector () { }
+        public void Decrease(int times)
+        {
+            if (times > 0)
+            {
+                Length = Length / times;
+            }
+            else { Console.WriteLine($"Can`t be reduced by {times} times"); }
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Colour: {LineColor}");
+            Console.WriteLine($"Angle: {Angle}°");
+            Console.WriteLine($"Length: {Length}");
+        }
+        public override string ToString()
+        {
+            return $"Vector (Colour: {LineColor}, Angle: {Angle}°, Length: {Length})";
+        }
+
+        public void Increase(int times) { Length = Length * times; }
+    }
+}
